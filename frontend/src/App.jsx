856a0d9bb65fc572a6de1e21ab52e3ec7c431722
nour.bassem <nour.bassem@egrobots.com>
2025-06-10 16:46:23 +0300

    added some styling and also learned the page routing concept

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
 import MovieCard from './components/MovieCard';
+import Favorites from './components/pages/favorites';
+import Favorite from './components/pages/favorites';
 import Home from "./components/pages/home"
+import NavBar from './components/NavBar';
+import { Route,Routes } from 'react-router-dom';
 // A component is really just any function in Javascript that returns some kind of jsx code
 // components starts with a capital letter
 // it needs to have some kind of parent level only one parent so we can make 2 divs in the same level
@@ -9,9 +13,16 @@ import Home from "./components/pages/home"
 function App() {
   return (
     //this is a fragment exactly the next line
-    <> 
-      <Home />
-    </>
+    // <></>
+    <div>
+      <NavBar />
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/favorites" element={<Favorites />}/>
+      </Routes>
+    </main>
+    </div>
   )
 }
 
---
+++ frontend/src/components/pages/favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites(){
+    return(
+        <div className="favorites-empty">
+            <h2>NO favorite movies Yet</h2>
+            <p> start adding movies to your favorites and they will appear here</p>
+        </div>
+    )
+}
+
+export default Favorites;
